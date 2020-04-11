0de3447a528717c3f2359de585fbd7ec26802e26
jimmymetts <jimmymetts@bellsouth.net>
2020-04-11 09:48:49 -0500

    used template literal to wrie

--- Main.js
+++ Main.js
@@ -95,5 +95,8 @@ const book1 = {
     title: 'Book 1',
     author: 'John Doe',
     year: '2013',
+    getSummary: function() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;  // uses Template literals to write sentence
+    }
 };
-console.log(book1.year);
+console.log(book1.getSummary());

fab1d7b8b59b4dd5b0e76cb465e1e57caad5f695
jimmymetts <jimmymetts@bellsouth.net>
2020-04-13 11:47:21 -0500

    added this.Author and used book2.revise to reverse string Jane to 'enaj'

--- prototypes.js
+++ prototypes.js
@@ -20,11 +20,25 @@ function Book(title, author, year) {
         return `${this.title} is ${years} years old`
     }
 
+// revise / Change Year
 
+Book.prototype.revise = function(newYear) {
+    this.Year = newYear;
+    this.revise = true;
+}
+
+//reverse string test?
+
+Book.prototype.revise = function(newAuthor) {
+    this.Author = newAuthor;
+    this.revise = true;
+}
 
 // Instantiate an Object
 
 const book1 = new Book('Book 1', 'John Doe', '2013');
 const book2 = new Book('Book 2', 'Jane Doe', '2016');
 
-console.log(book2.getAge());
+console.log(book2);
+book2.revise('enaj');
+console.log(book2);

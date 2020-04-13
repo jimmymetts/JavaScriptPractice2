287c5710033665e3c5efa1a22e1f4bb3b9244442
jimmymetts <jimmymetts@bellsouth.net>
2020-04-13 09:35:39 -0500

    added this.getSummary to function book

--- constructors.js
+++ constructors.js
@@ -4,6 +4,9 @@ function Book(title, author, year) {
     this.author = author;
     this.year = year;
     
+    this.getSummary = function() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;  // uses Template literals to write sentence
+    }
     
 
 };
@@ -12,3 +15,5 @@ function Book(title, author, year) {
 
 const book1 = new Book('Book 1', 'John Doe', '2013');
 const book2 = new Book('Book 2', 'Jane Doe', '2016');
+
+console.log(book1);

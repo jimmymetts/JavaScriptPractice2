3a13cb6667f9b9ab27df45f9601b0c4ed6d753e6
jimmymetts <jimmymetts@bellsouth.net>
2020-04-13 11:51:11 -0500

    added reverseTest page

---
+++ reverseTest.js
@@ -0,0 +1,15 @@
+// reverse string test
+
+Book.prototype.revise = function(newAuthor) {
+    this.Author = newAuthor;
+    this.revise = true;
+}
+
+// Instantiate an Object
+
+const book1 = new Book('Book 1', 'John Doe', '2013');
+const book2 = new Book('Book 2', 'Jane Doe', '2016');
+
+console.log(book2);
+book2.revise('enaj');
+console.log(book2);

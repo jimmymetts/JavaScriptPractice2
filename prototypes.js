1855d65eb917b7ae89fe20f42563f3100afc8aeb
jimmymetts <jimmymetts@bellsouth.net>
2020-04-13 11:07:21 -0500

    added prototypes.js to script in html

--- prototypes.js
+++ prototypes.js
@@ -11,13 +11,20 @@ function Book(title, author, year) {
 
     Book.prototype.getSummary = function () {
         return `${this.title} was written by ${this.author} in ${this.year}`;  
+    };
+
+    // getAge
+
+    Book.prototype.getAge = function () {
+        const years = new Date().getFullYear - this.year;
+        return `${this.title} is ${years} old`
     }
 
-};
+
 
 // Instantiate an Object
 
 const book1 = new Book('Book 1', 'John Doe', '2013');
 const book2 = new Book('Book 2', 'Jane Doe', '2016');
 
-console.log(book1.getSummary());
+console.log(book2);

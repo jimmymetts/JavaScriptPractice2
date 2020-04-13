4da9196e39ab5576e97b5ad0cc48cf87d599fd8d
jimmymetts <jimmymetts@bellsouth.net>
2020-04-13 09:20:50 -0500

    added 'Book 2', 'Jane Doe', '2016' to const book2

--- constructors.js
+++ constructors.js
@@ -11,4 +11,4 @@ function Book(title, author, year) {
 // Instantiate an Object
 
 const book1 = new Book('Book 1', 'John Doe', '2013');
-const book2 = new Book();
+const book2 = new Book('Book 2', 'Jane Doe', '2016');

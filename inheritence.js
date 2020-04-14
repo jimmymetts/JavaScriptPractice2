575c7c6306e8d1f06f604c047d8e57d04135a80f
jimmymetts <jimmymetts@bellsouth.net>
2020-04-14 14:40:32 -0500

    added function Book & Book.prototype to inheritence.js

---
+++ inheritence.js
@@ -0,0 +1,14 @@
+// constructors
+function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+}
+
+//getSummary
+    Book.prototype.getSummary = function() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;  // uses Template literals to write sentence
+    }
+    
+
+};
